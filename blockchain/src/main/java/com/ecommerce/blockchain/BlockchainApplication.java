d631854a7a278965149901d5ccefde739a4894cf
dawit95 <dawit0310@gmail.com>
2021-09-13 16:44:53 +0900

    [fix]: 컴파일 에러수정/BE

---
+++ blockchain/src/main/java/com/ecommerce/blockchain/BlockchainApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.blockchain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockchainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlockchainApplication.class, args);
+    }
+
+}
