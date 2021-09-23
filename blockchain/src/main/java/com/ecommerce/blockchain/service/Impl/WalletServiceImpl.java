a0bd1da95132157840cfe0872ebf66c2e0fdbb29
lim8662 <lim8662@gmail.com>
2021-09-23 15:36:05 +0900

    [feat] : 트랜잭션 생성 및 이더 송금 /BE

--- blockchain/src/main/java/com/ecommerce/blockchain/service/Impl/WalletServiceImpl.java
+++ blockchain/src/main/java/com/ecommerce/blockchain/service/Impl/WalletServiceImpl.java
@@ -12,6 +12,7 @@ import org.web3j.protocol.core.DefaultBlockParameterName;
 import org.web3j.protocol.core.methods.request.Transaction;
 import org.web3j.protocol.core.methods.response.EthCoinbase;
 import org.web3j.protocol.core.methods.response.EthGetTransactionCount;
+import org.web3j.protocol.core.methods.response.EthSendTransaction;
 import org.web3j.protocol.core.methods.response.Web3ClientVersion;
 import org.web3j.protocol.http.HttpService;
 import org.web3j.utils.Convert;
@@ -26,10 +27,14 @@ import java.util.concurrent.ExecutionException;
 @Service
 public class WalletServiceImpl implements WalletService{
 
-    Web3j web3j = Web3j.build(new HttpService("http://j5a4071.p.ssafy.io:5407"));
+    Web3j web3j = Web3j.build(new HttpService("http://j5a4071.p.ssafy.io:54072"));
 
     //계정 관리 API 사용 ex) 지갑의 unlock
-    Admin admin = Admin.build(new HttpService("http://j5a4071.p.ssafy.io:5407"));
+    Admin admin = Admin.build(new HttpService("http://j5a4071.p.ssafy.io:54072"));
+
+    BigInteger GAS_PRICE = BigInteger.valueOf(154000000000L);
+
+    BigInteger GAS_LIMIT = BigInteger.valueOf(100000);
 
     @Override
     public WalletResponseDto getWallet(String address) throws IOException {
@@ -44,16 +49,23 @@ public class WalletServiceImpl implements WalletService{
         WalletResponseDto walletDto = new WalletResponseDto();
         walletDto.setAddress(address);
         walletDto.setBalance(ethBalance);
+
+        // DB의 지갑 정보 업데이트해서 리턴
+
+
         return walletDto;
     }
-
+    // 해당 계정에 10이더 충전
     @Override
     public WalletResponseDto chargeEther(String address) throws IOException, ExecutionException, InterruptedException {
 
+        // 충전량 제한하여 충전
+        // user 및 wallet DB 필요
+        
         // 마이닝한 이더를 가지고 있는 코인베이스 계정을 조회
         EthCoinbase coinbase = web3j.ethCoinbase().sendAsync().get();
         String coinbaseAddr = coinbase.getAddress();
-        //System.out.println(coinbaseAddr);
+        System.out.println(coinbaseAddr);
 
         // 코인베이스 계정의 lock 해제
         PersonalUnlockAccount personalUnlockAccount = admin.personalUnlockAccount(coinbaseAddr, "eth").send();
@@ -83,15 +95,26 @@ public class WalletServiceImpl implements WalletService{
         System.out.println(nonce);
 
         // 충전할 이더량 wei로 환산
-        BigInteger transferAmountWei =
+        BigInteger value =
         Convert.toWei("10", Convert.Unit.ETHER).toBigIntegerExact();
-        System.out.println(transferAmountWei);
+        System.out.println(value);
 
+        // wei를 이더로 환산
+//        int result = Integer.parseInt( Convert.fromWei(value.toString() , Convert.Unit.ETHER).toString() );
+//        System.out.println(result);
+
+        // 트랜잭션 생성
 //        RawTransaction rawTransaction  = RawTransaction.createEtherTransaction(
-//                nonce, <gas price>, <gas limit>, <toAddress>, <value>);
+//                nonce, GAS_PRICE, GAS_LIMIT, address, value);
 
+        Transaction transaction =
+        Transaction.createFunctionCallTransaction(coinbaseAddr, nonce, GAS_PRICE, GAS_LIMIT, address, value, null);
+        EthSendTransaction ethSendTransaction = web3j.ethSendTransaction(transaction).sendAsync().get();
 
+        System.out.println(ethSendTransaction.getTransactionHash());
         WalletResponseDto walletDto = new WalletResponseDto();
-        return walletDto;
+
+
+        return getWallet(address);
     }
 }
