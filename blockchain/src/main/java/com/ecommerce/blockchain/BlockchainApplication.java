667979c39328767e639e70fb9e21961d082a8d7c
dawit95 <dawit0310@gmail.com>
2021-10-01 13:58:20 +0900

    [feat] : add NFT ERC721 abi&bin&wrapperClass/BE

--- blockchain/src/main/java/com/ecommerce/blockchain/BlockchainApplication.java
+++ blockchain/src/main/java/com/ecommerce/blockchain/BlockchainApplication.java
@@ -1,6 +1,7 @@
 package com.ecommerce.blockchain;
 
 import com.ecommerce.blockchain.service.ERC20Service;
+import com.ecommerce.blockchain.service.ERC721Service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,8 +10,10 @@ public class BlockchainApplication {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(BlockchainApplication.class, args);
-        ERC20Service erc20 = new ERC20Service();
-        erc20.deployERC20();
+        ERC721Service erc721 = new ERC721Service();
+//        erc721.deployERC721();
+        erc721.mintImageERC721Test();
+
 //        erc20.loadERC20();
 //        erc20.transferERC20Test();
     }
