99eb737e206333b4ed877ec46340e398dac18efd
dawit95 <dawit0310@gmail.com>
2021-10-04 11:23:40 +0900

    [feat] : nft entity /BE

---
+++ blockchain/src/main/java/com/ecommerce/blockchain/api/NFTController.java
@@ -0,0 +1,17 @@
+package com.ecommerce.blockchain.api;
+
+import com.ecommerce.blockchain.domain.user.User;
+import com.ecommerce.blockchain.domain.wallet.WalletRegistReq;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/nft")
+public class NFTController {
+
+
+}
---
+++ blockchain/src/main/java/com/ecommerce/blockchain/domain/nft/NFT.java
@@ -0,0 +1,39 @@
+package com.ecommerce.blockchain.domain.nft;
+
+import com.ecommerce.blockchain.domain.BaseTime;
+import com.ecommerce.blockchain.domain.user.User;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class NFT extends BaseTime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JsonManagedReference
+    @ManyToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+    @Column(name = "seller_id",nullable = false)
+//    @JoinColumn(name="seller_id")
+    private User user;
+
+    @Column(nullable = false, columnDefinition = "LONGTEXT")
+    private String imageUrl;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = true)
+    private String category;
+
+    @Column(nullable = true)
+    private String explanation;
+}
