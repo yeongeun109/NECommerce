9fdbacedbb4d70a2df0e2e79f1faf5ba7833aa62
윤영은 <ounog@naver.com>
2021-10-07 01:00:04 +0900

    [test] : ownerOf() test /FE

--- src/assets/scripts/transfer-nft.js
+++ src/assets/scripts/transfer-nft.js
@@ -1,108 +1,101 @@
-import React from 'react';
-import contract from "./MyItem.json";
+import React from "react";
+import BigNumber from "bignumber.js";
 import axios from "axios";
+import jwt from "jsonwebtoken";
+import {Router} from "react-router-dom";
 
-const TransactNFT = (nftName, imgURI, intro, category) => {
+const TransferNFT = async (owner, price, tokenId) => {
 
-    require("dotenv").config();
     const Web3 = require('web3')
-    const {REACT_APP_API_URL, REACT_APP_PRIVATE_KEY, REACT_APP_PUBLIC_KEY, REACT_APP_MY_AMAZON_SERVER, REACT_APP_SERVER_BASE_URL} = process.env;
-
-    // const provider = require("eth-provider");
-    // const web3 = new Web3(provider(REACT_APP_API_URL))
-
-    const contract = require("./MyItem.json")
-
+    const {REACT_APP_API_URL, REACT_APP_PUBLIC_KEY} = process.env;
     let web3js
 
     if (typeof web3 !== 'undefined') {
         web3js = new Web3(window.web3.currentProvider);
         console.log("web3 okay");
     } else {
-        web3js = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161"));
+        web3js = new Web3(new Web3.providers.HttpProvider(REACT_APP_API_URL));
         console.log("web3 undefined");
     }
 
-    //const contractAddress = "0x065CA34BECe8BcF281e87AECda088ef10c00168A" // 영은
-    const contractAddress = "0x72fc26DEc1554b816C39e12601534e92aF292898"; // 영찬
-    const nftContract = new web3js.eth.Contract(contract.abi, contractAddress)
+    const neContractABI = require("./NeERC20.json")
+    const neContractAddress = "0xDA4a547a5622fc3D592F0144fBCae2754679245e"
+    const NEContract = new web3js.eth.Contract(neContractABI.abi, neContractAddress)
 
-    async function mintNFT(nftName, imgURI, intro, category) {
+    const nftContractABI = require("./MyItem.json")
+    const nftContractAddress = "0x9d83e140330758a8fFD07F8Bd73e86ebcA8a5692"
+    const nftContract = new web3js.eth.Contract(nftContractABI.abi, nftContractAddress)
 
-        let account = await web3js.eth.getAccounts().then(function (array) {
-            return array[0]
-        });
-        console.log(nftName)
-        console.log(imgURI)
-        console.log(intro)
-        console.log(category)
-        console.log(account)
+    // nftContract.methods.ownerOf(tokenId).call().then(
+    //
+    // );
+    // Router.push(`/campaigns/${REACT_APP_PUBLIC_KEY}/requests`)
+    nftContract.methods.ownerOf(0).call(function (err, res) {
+        if (err) {
+            console.log("An error occured", err)
+            return
+        }
+        console.log("The balance is: ", res.address)
+    })
 
-        const nonce = await web3js.eth.getTransactionCount(account, 'latest'); //get latest nonce
-        let tx_hash;
 
-        web3js.eth.sendTransaction({
-            'from': account,
-            'to': contractAddress,
-            'nonce': nonce,
-            'gas': 500000,
-            'maxPriorityFeePerGas': 1999999987,
-            'input': nftContract.methods.mintImage(nftName, account, "ddd.jpg", "explane", 0).encodeABI()
-        },function(error, hash){
-            tx_hash = hash;
-            console.log(tx_hash);
-        }).then({
-
-        });
-
-        console.log(account)
-        // const signPromise = web3js.eth.accounts.signTransaction(tx, REACT_APP_PRIVATE_KEY);
-
-        // signPromise.then((signedTx) => {
-        //
-        //     web3js.eth.sendSignedTransaction(signedTx.rawTransaction, function(err, hash) {
-        //     if (!err) {
-        //         console.log("The hash of your transaction is: ", hash);
-        //     } else {
-        //         console.log("Something went wrong when submitting your transaction:", err)
-        //     }
-        //     });
-        // }).catch((err) => {
-        //     console.log("Promise failed:", err);
-        // });
-
-        //리턴값 받아서 BE에 전달(해시값 이랑 NFT 정보)
-        let token = window.localStorage.getItem("token");
-        const formData = {address:account, ownerId:1}
-        //const formData = {email:"test1@naver.com", password:"ssafy407!"}
-        console.log(JSON.parse(window.localStorage.getItem("token")))
-        //api/v1/wallet/token
-
-        axios
-            .put(
-                'api/v1/wallet/token',
-                formData,
-                {
-                    headers: {
-                        token:
-                            JSON.parse(window.localStorage.getItem("token")),
-                        "Content-Type": `multipart/form-data`,
-                    },
-                }
-            )
-            .then((response) => {
-                alert("상품 등록에 성공하였습니다");
-                //history.push(`/detail/${response.data}`);
-                //response.data = NFT의 ID값
-            })
-            .catch((error) => {
-                alert("상품을 등록하지 못햇습니다");
-            });
-
-    }
-
-    mintNFT(nftName, imgURI, intro, category)
+    // let account = await web3js.eth.getAccounts().then(function (array) {
+    //     return array[0]
+    // });
+    //
+    // let token = JSON.parse(window.localStorage.getItem("token"));
+    // token = token.replace('Bearer', "")
+    // let jwt = require('jsonwebtoken')
+    // let uid = jwt.decode(token).uid;
+    // console.log("uid : ", uid)
+    //
+    // const formData = {buyerId:uid, nftId:tokenId}
+    //
+    // const nonce = await web3js.eth.getTransactionCount(account, 'latest'); //get latest nonce
+    //
+    // async function transferNFT() {
+    //     web3js.eth.sendTransaction({
+    //         'from': account,
+    //         'to': nftContractAddress,
+    //         'nonce': nonce,
+    //         'gas': 500000,
+    //         'input': nftContract.methods.transferFrom(ownerAddress, account, tokenId).encodeABI()
+    //     }).then(() => {
+    //         console.log("nft transfer 오케이")
+    //         axios
+    //             .post(
+    //                 '/api/v1/product/purchase',
+    //                 formData
+    //             )
+    //             .then((response) => {
+    //                 alert("구매 성공했습니다.");
+    //             })
+    //             .catch((error) => {
+    //                 alert("구매 실패했습니다.");
+    //             })
+    //     }).catch(() => {
+    //     })
+    // }
+    //
+    // async function transferNE() {
+    //     let amount = BigNumber(price * Math.pow(10, 18))
+    //     web3js.eth.sendTransaction({
+    //         'from': account,
+    //         'to': neContractAddress,
+    //         'nonce': nonce,
+    //         'gas': 500000,
+    //         'maxPriorityFeePerGas': 1999999987,
+    //         'input': NEContract.methods.transfer(ownerAddress, amount).encodeABI()
+    //     }, function (error, hash) {
+    //         console.log(hash);
+    //     }).then(() => {
+    //         console.log("오케이")
+    //     }).catch(() => {
+    //     })
+    // }
+    //
+    // transferNE()
+    // transferNFT()
 }
 
-// mintNFT("https://myimageuploadserver.s3.ap-northeast-2.amazonaws.com/nft-metadata.json")
-export default TransactNFT
+export default TransferNFT
--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -4,6 +4,7 @@ import OnboardingButton from "../components/OnboardingMetamask";
 import { Tabs, Tab } from "react-bootstrap";
 import MyWallet from "../components/MyWallet";
 import OwningNFT from "../components/OwningNFT";
+
 const MyPage = () => {
     const [key, setKey] = useState("wallet")
     const {status, connect, account } = useMetaMask();
@@ -12,9 +13,9 @@ const MyPage = () => {
     // if (status === "notConnected") return <button onClick={connect}>메타마스크연결</button>
     // if (status === "connecting") return <div>커넥팅</div>
     // if (status === "connected") return <div>Connected account : {account}</div>
+
     return (
         <div>
-
             {(status !== "connected") ?
             (<div className="d-flex justify-content-center">
                 <OnboardingButton/>    
@@ -28,7 +29,6 @@ const MyPage = () => {
                 <Tab title="My NFT" eventKey="MyNFT">
                     <OwningNFT/>
                 </Tab>
-
                 {/* <div>Connected account : {account}</div> */}
             </Tabs>
             )
