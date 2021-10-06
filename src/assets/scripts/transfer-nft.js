03acd62c515640e435813e6ba11c880874cdd415
윤영은 <ounog@naver.com>
2021-10-07 02:42:42 +0900

    [test] : ownerOf() test /FE

--- src/assets/scripts/transfer-nft.js
+++ src/assets/scripts/transfer-nft.js
@@ -30,72 +30,72 @@ const TransferNFT = async (owner, price, tokenId) => {
     //
     // );
     // Router.push(`/campaigns/${REACT_APP_PUBLIC_KEY}/requests`)
-    nftContract.methods.ownerOf(0).call(function (err, res) {
-        if (err) {
-            console.log("An error occured", err)
-            return
-        }
-        console.log("The balance is: ", res.address)
-    })
+    // nftContract.methods.ownerOf(0).call(function (err, res) {
+    //     if (err) {
+    //         console.log("An error occured", err)
+    //         return
+    //     }
+    //     console.log("The balance is: ", res.address)
+    // })
 
 
-    // let account = await web3js.eth.getAccounts().then(function (array) {
-    //     return array[0]
-    // });
-    //
-    // let token = JSON.parse(window.localStorage.getItem("token"));
-    // token = token.replace('Bearer', "")
-    // let jwt = require('jsonwebtoken')
-    // let uid = jwt.decode(token).uid;
-    // console.log("uid : ", uid)
-    //
-    // const formData = {buyerId:uid, nftId:tokenId}
-    //
-    // const nonce = await web3js.eth.getTransactionCount(account, 'latest'); //get latest nonce
-    //
-    // async function transferNFT() {
-    //     web3js.eth.sendTransaction({
-    //         'from': account,
-    //         'to': nftContractAddress,
-    //         'nonce': nonce,
-    //         'gas': 500000,
-    //         'input': nftContract.methods.transferFrom(ownerAddress, account, tokenId).encodeABI()
-    //     }).then(() => {
-    //         console.log("nft transfer 오케이")
-    //         axios
-    //             .post(
-    //                 '/api/v1/product/purchase',
-    //                 formData
-    //             )
-    //             .then((response) => {
-    //                 alert("구매 성공했습니다.");
-    //             })
-    //             .catch((error) => {
-    //                 alert("구매 실패했습니다.");
-    //             })
-    //     }).catch(() => {
-    //     })
-    // }
-    //
-    // async function transferNE() {
-    //     let amount = BigNumber(price * Math.pow(10, 18))
-    //     web3js.eth.sendTransaction({
-    //         'from': account,
-    //         'to': neContractAddress,
-    //         'nonce': nonce,
-    //         'gas': 500000,
-    //         'maxPriorityFeePerGas': 1999999987,
-    //         'input': NEContract.methods.transfer(ownerAddress, amount).encodeABI()
-    //     }, function (error, hash) {
-    //         console.log(hash);
-    //     }).then(() => {
-    //         console.log("오케이")
-    //     }).catch(() => {
-    //     })
-    // }
-    //
-    // transferNE()
-    // transferNFT()
+    let account = await web3js.eth.getAccounts().then(function (array) {
+        return array[0]
+    });
+
+    let token = JSON.parse(window.localStorage.getItem("token"));
+    token = token.replace('Bearer', "")
+    let jwt = require('jsonwebtoken')
+    let uid = jwt.decode(token).uid;
+    console.log("uid : ", uid)
+
+    const formData = {buyerId:uid, nftId:tokenId}
+
+    const nonce = await web3js.eth.getTransactionCount(account, 'latest'); //get latest nonce
+
+    async function transferNFT() {
+        web3js.eth.sendTransaction({
+            'from': account,
+            'to': nftContractAddress,
+            'nonce': nonce,
+            'gas': 500000,
+            'input': nftContract.methods.transferFrom(ownerAddress, account, tokenId).encodeABI()
+        }).then(() => {
+            console.log("nft transfer 오케이")
+            axios
+                .post(
+                    '/api/v1/product/purchase',
+                    formData
+                )
+                .then((response) => {
+                    alert("구매 성공했습니다.");
+                })
+                .catch((error) => {
+                    alert("구매 실패했습니다.");
+                })
+        }).catch(() => {
+        })
+    }
+
+    async function transferNE() {
+        let amount = BigNumber(price * Math.pow(10, 18))
+        web3js.eth.sendTransaction({
+            'from': account,
+            'to': neContractAddress,
+            'nonce': nonce,
+            'gas': 500000,
+            'maxPriorityFeePerGas': 1999999987,
+            'input': NEContract.methods.transfer(ownerAddress, amount).encodeABI()
+        }, function (error, hash) {
+            console.log(hash);
+        }).then(() => {
+            console.log("오케이")
+        }).catch(() => {
+        })
+    }
+
+    transferNE()
+    transferNFT()
 }
 
 export default TransferNFT
