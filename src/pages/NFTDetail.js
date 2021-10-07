01813b0740e7099442bb8d68f93841e1816d1865
윤영은 <ounog@naver.com>
2021-10-07 15:21:52 +0900

    [fix] : fix NFTDetail.js /FE

--- src/pages/NFTDetail.js
+++ src/pages/NFTDetail.js
@@ -33,33 +33,62 @@ const NFTDetail = (props,{history}) => {
     };
     fetchData();
   }, []);
-    
+
+  const cardStyle ={
+      width: "70%",
+      //
+      // maxHeight: "40vh",
+      // maxWidth: "40vw"
+  }
   return (
-    <div>
-      <Container>
-        <Row>
-          <Col>
-            <Image src={NFTDetail.imageUrl} height={500} id="imageUrl"/>
-          </Col>
-          <Col>
-            <div>
-              상품이름 : {NFTDetail.title}
+    <div style={cardStyle}>
+        <div className="card mb-3" >
+            <h3 className="card-header">{NFTDetail.title}</h3>
+            <div className="card-body">
+                <span className="badge bg-dark">{NFTDetail.category === "0" ? "Art" : "Photo"}</span>
             </div>
-            <div>
-              상품 설명 : {NFTDetail.explanation}
+            {/*<svg xmlns="http://www.w3.org/2000/svg" className="d-block user-select-none" width="100%" height="200"*/}
+            {/*     aria-label="Placeholder: Image cap" focusable="false" role="img" preserveAspectRatio="xMidYMid slice"*/}
+            {/*     viewBox="0 0 318 180" style="font-size:1.125rem;text-anchor:middle">*/}
+            {/*    <rect width="100%" height="100%" fill="#868e96"></rect>*/}
+            {/*    */}
+            {/*</svg>*/}
+            <img src={NFTDetail.imageUrl} className="m-2" />
+            <ul className="list-group list-group-flush">
+                <li className="list-group-item"><b>상품 설명</b></li>
+                <li className="list-group-item">{NFTDetail.explanation}</li>
+            </ul>
+            <div className="card-body">
+                <button type="button" className="btn btn-primary">판매등록하기</button>
             </div>
-          </Col>
-        </Row>
-        <Row>
-          <div>
-            거래내역
-            <span>{NFTDetail.transactionHistory}</span>
-          </div>
-          <div>
-            
-          </div>
-        </Row>
-      </Container>
+            <div className="card-footer text-muted">
+                {NFTDetail.transactionHash}
+            </div>
+        </div>
+      {/*<Container>*/}
+      {/*  <Row>*/}
+      {/*    <Col>*/}
+      {/*      <Image src={NFTDetail.imageUrl} height={500} id="imageUrl"/>*/}
+      {/*    </Col>*/}
+      {/*    <Col>*/}
+      {/*      <div>*/}
+      {/*        상품이름 : {NFTDetail.title}*/}
+      {/*      </div>*/}
+      {/*      <div>*/}
+      {/*        상품 설명 : {NFTDetail.explanation}*/}
+      {/*      </div>*/}
+      {/*    </Col>*/}
+      {/*  </Row>*/}
+      {/*  <Row>*/}
+      {/*    <div>*/}
+      {/*      거래내역*/}
+      {/*      <span>{NFTDetail.transactionHistory}</span>*/}
+      {/*    </div>*/}
+      {/*    <div>*/}
+      {/*      */}
+      {/*    </div>*/}
+      {/*  </Row>*/}
+      {/*</Container>*/}
     </div>
   );
 
