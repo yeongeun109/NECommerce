f9c9cb69dd3cf03e712c9d957b789949e211c5f3
윤영은 <ounog@naver.com>
2021-10-07 23:43:28 +0900

    [feat] : NE 잔액 추가 /FE

--- src/components/GetAccount.js
+++ src/components/GetAccount.js
@@ -3,42 +3,43 @@ import React, { useState } from 'react';
 import { Button, InputGroup, FormControl } from 'react-bootstrap'
 import { useMetaMask } from "metamask-react";
 const GetAccount = (props) => {
-  console.log(props.account)
-  const [baseBalance, setBaseBalance] = useState("")
+  // console.log(props.account)
   const [addressBalance, setAddressBalance] = useState("")
   const [address, setAddress] = useState(props.account)
+  const [neBalance, setNeBalance] = useState("")
   
   async function baseAccountBalance () {
     web3.eth.getBalance(address, function(error, wei){
       if(!error) {
-        var balance = web3.utils.fromWei(wei, "ether")
+        let balance = web3.utils.fromWei(wei, "ether")
         // console.log(balance)
-        setAddressBalance(balance + "ETH")
+        setAddressBalance(balance + " ETH")
       }
     })
   }
 
-  const onChangeInput = (event) => {
-    setAddress(event.target.value);
+  async function getERC20TokenBalance () {
+    const ne = require("../assets/scripts/NeERC20.json")
+    const contractAddress = "0xDA4a547a5622fc3D592F0144fBCae2754679245e";
+
+    // let contract = web3.eth.contract(myItem.abi).at(contractAddress);
+    const contract = new web3.eth.Contract(ne.abi, contractAddress)
+    setNeBalance(await contract.methods.balanceOf(props.account).call());
+    // console.log("neBalance : " + neBalance)
   }
 
-  const changeAddress = () => {
-    web3.eth.getBalance(address, function(error, wei){
-      if(!error) {
-        var balance = web3.utils.fromWei(wei, "ether")
-        // console.log(balance)
-        setAddressBalance(balance + "ETH")
-      }
-    })
-  }
-
+  getERC20TokenBalance()
+  baseAccountBalance()
   return (
     <div className="get-account">
       <div>
-        현재 연결된 계정은 {props.account}입니다.
+        현재 연결된 계정 {props.account}
       </div>
       <div>
-        현재 계정에는 {addressBalance}
+        현재 ETH 잔액 {addressBalance}
+      </div>
+      <div>
+        현재 NE 잔액 {neBalance} NE
       </div>
     </div>
   );
