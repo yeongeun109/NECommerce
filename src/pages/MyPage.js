6300f5794aae3c038465609412261f975bc5c6df
WookiJung <wkjung0329@gmail.com>
2021-10-07 15:20:24 +0900

    [fix]:mainpage 수정/FE

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useMetaMask } from "metamask-react";
 import OnboardingButton from "../components/OnboardingMetamask";
 import { Tabs, Tab } from "react-bootstrap";
@@ -7,34 +7,28 @@ import OwningNFT from "../components/OwningNFT";
 
 const MyPage = () => {
     const [key, setKey] = useState("wallet")
-    const {status, account } = useMetaMask();
-    // if (status === "initializing") return <div>싱크로 진행중</div>
-    // if (status === "unavailable") return <div>메타마스크 사용불가</div>
-    // if (status === "notConnected") return <button onClick={connect}>메타마스크연결</button>
-    // if (status === "connecting") return <div>커넥팅</div>
-    // if (status === "connected") return <div>Connected account : {account}</div>
+    const {status, connect, account } = useMetaMask();
+                
 
     return (
         <div>
-            {(status !== "connected") ?
-            (<div className="d-flex justify-content-center">
-                <OnboardingButton/>    
-            </div>)
-                :
+            {(status !== "connected") ? 
+            (<div><OnboardingButton/></div>) 
+            : 
             (
-            <Tabs activeKey={key} onSelect={(k) => setKey(k)} id="my-page">
-                <Tab title="Wallet" eventKey="wallet">
-                    <MyWallet account={account}/>
-                </Tab>
-                <Tab title="My NFT" eventKey="MyNFT">
-                    <OwningNFT/>
-                </Tab>
-                {/* <div>Connected account : {account}</div> */}
-            </Tabs>
-            )
-            }
+            <div>
+    
+                <Tabs activeKey={key} onSelect={(k) => setKey(k)} id="my-page">
+                    <Tab title="Wallet" eventKey="wallet">
+                        <MyWallet account={account}/>
+                    </Tab>
+                    <Tab title="My NFT" eventKey="MyNFT">
+                        <OwningNFT/>
+                    </Tab>
+                </Tabs>
+            </div>)}
         </div>
-    )
-}
+        );
+};
 
 export default MyPage
