37edbca47b95493b15fc1f3aef4a1d3794207101
dawit95 <dawit0310@gmail.com>
2021-10-08 03:26:02 +0900

    [fix] modify error /fe

--- src/assets/scripts/transfer-nft.js
+++ src/assets/scripts/transfer-nft.js
@@ -83,7 +83,7 @@ const TransferNFT = async (price, tokenId, nftId, userId, productId) => {
   }
 
   async function transferNE() {
-    let amount = price * Math.pow(10, 18);
+    let amount = price ;
     web3js.eth
       .sendTransaction(
         {
